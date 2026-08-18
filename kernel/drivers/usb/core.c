3a17b5c805d7e158247525669599bc889ba5af0a
zhaoyangzhangus <zhangzhaoyangus@icloud.com>
2026-08-18 08:02:15 +0800

    update

---
+++ kernel/drivers/usb/core.c
@@ -0,0 +1,64 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <usb/device.h>
+
+
+#define USB_MAX_DEVICES 256
+
+
+static usb_device_t
+usb_devices[
+    USB_MAX_DEVICES];
+
+
+
+usb_device_t *
+usb_get_device(
+    uint8_t slot)
+{
+    if(slot == 0)
+    {
+        return 0;
+    }
+
+
+    if(!usb_devices[slot].used)
+    {
+        return 0;
+    }
+
+
+    return
+        &usb_devices[slot];
+}
+
+
+
+usb_device_t *
+usb_alloc_device(
+    uint8_t slot)
+{
+    if(slot == 0)
+    {
+        return 0;
+    }
+
+
+    usb_device_t *dev =
+        &usb_devices[slot];
+
+
+    __builtin_memset(
+        dev,
+        0,
+        sizeof(*dev));
+
+
+    dev->used = true;
+
+    dev->slot_id = slot;
+
+
+    return dev;
+}
